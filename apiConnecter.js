fd9e5c5c35e9b1846329d0201ef6d78f5b3834b0
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2025-04-27 01:23:46 +0530

    Update apiConnecter.js

--- apiConnecter.js
+++ apiConnecter.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+require('dotenv').config(); 
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/authApp", // Updated base URL
+  baseURL: process.env.API_URL || "http://localhost:5000/api/v1/authApp", // Updated base URL
 });
 
 API.interceptors.request.use((req) => {
