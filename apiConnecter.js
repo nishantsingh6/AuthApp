baebd084db17e7a9311ac87d885a59d46d4800d8
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2025-04-27 13:31:52 +0530

    Update apiConnecter.js

--- apiConnecter.js
+++ apiConnecter.js
@@ -2,7 +2,7 @@ import axios from "axios";
  
 
 const API = axios.create({
-  baseURL: "https://auth-app-bmpl.onrender.com/api/v1/authApp", // Updated base URL
+  baseURL: `${process.env.API_URL}/api/v1/authApp/`, // Updated base URL
 });
 
 API.interceptors.request.use((req) => {
