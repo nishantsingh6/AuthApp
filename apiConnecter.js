353f16165ceca9a2ad2141af47b1806b85c82850
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2025-04-27 14:18:04 +0530

    Update apiConnecter.js

--- apiConnecter.js
+++ apiConnecter.js
@@ -5,7 +5,7 @@ console.log('Vite API URL:', process.env.VITE_API_URL);
  // Check the value of your base URL
 
 const API = axios.create({
-  baseURL: process.env.VITE_API_URL, // This will now have the full URL
+  baseURL:"https://auth-app-bmpl.onrender.com/api/v1/authApp", // This will now have the full URL
 });
 
 
