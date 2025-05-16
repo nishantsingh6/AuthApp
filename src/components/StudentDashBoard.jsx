baca0577673000a091403c51509701a9ce3aad90
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2025-05-16 23:13:57 +0530

    Update StudentDashBoard.jsx

--- src/components/StudentDashBoard.jsx
+++ src/components/StudentDashBoard.jsx
@@ -103,7 +103,7 @@ const StudentDashboard = () => {
             <button
               className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
               onClick={() => {
-                localStorage.delete('token');
+                localStorage.removeItem('token');
                 // window.location.href = '/login';
               }}
             >
