a30cfe06b2a6a19f3cf0f684b6f279dfb2543fa1
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2025-05-16 23:16:44 +0530

    Update AdminDashBoard.jsx

--- src/components/AdminDashBoard.jsx
+++ src/components/AdminDashBoard.jsx
@@ -108,7 +108,7 @@ const AdminDashboard = () => {
             <button
               className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
               onClick={() => {
-                localStorage.delete('token');
+                localStorage.removeItem('token');
                 // window.location.href = '/login';
               }}
             >
